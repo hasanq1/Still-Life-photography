83fabbe32ab76240b50ba2528ccad9fef1c2e46d
Hasan Qureshi <hasanq36@gmail.com>
2022-10-26 20:18:45 -0500

    Upaded push

---
+++ components/Footer/index.js
@@ -0,0 +1,36 @@
+import React from "react";
+import Socials from "../Socials";
+import Link from "next/link";
+import Button from "../Button";
+
+const Footer = ({}) => {
+  return (
+    <>
+      <div className="mt-5 laptop:mt-40 p-2 laptop:p-0">
+        <div>
+          <h1 className="text-2xl text-bold">Contact.</h1>
+          <div className="mt-10">
+            <h1 className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl text-bold">
+              Let&apos;s capture your best moments
+            </h1>
+            <h1 className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl text-bold">
+              Together.
+            </h1>
+            <Button type="primary">Schedule a Session</Button>
+            <div className="mt-10">
+              <Socials />
+            </div>
+          </div>
+        </div>
+      </div>
+      <h1 className="text-sm text-bold mt-2 laptop:mt-10 p-2 laptop:p-0">
+        Made with ☕️ by{" "}
+        <Link href="https://www.instagram.com/kingdadrianproductions/">
+          <a className="underline underline-offset-1">Still life Photography</a>
+        </Link>
+      </h1>
+    </>
+  );
+};
+
+export default Footer;
